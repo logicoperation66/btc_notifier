f4b42902fdac3e8a93a21a17da4754b696160d83
Adam Wawrzyniak <logicoperation@gmail.com>
2024-02-28 15:12:54 +0100

    tl;dr

--- statistics_func/statistics.py
+++ statistics_func/statistics.py
@@ -4,4 +4,4 @@ from coinranking import usd_exchange_rate, btc_exchange_price
 def price_calculate() -> float:
     usd_price = usd_exchange_rate()
     btc_price = btc_exchange_price()
-    return float(round(usd_price*btc_price, 2))
+    return round(usd_price*btc_price, 2)
